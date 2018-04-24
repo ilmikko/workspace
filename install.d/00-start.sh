ee3bdbe35959e90f766171325681814180e86f84
ilmikko <mikko@5x.fi>
2018-04-24 22:18:03 +0100

    Check shell at the start of the installation

--- install.d/00-start.sh
+++ install.d/00-start.sh
@@ -1,5 +1,15 @@
 # 0. Config
 # Calls all necessary installation files, and guides through the installation process.
 
+# Determine if we are in a correct shell; the installer will run nevertheless, but
+# it will warn that there might be consequences as I haven't tested all shells yet.
+OOS_SHELL=$(get_shell);
+
+debug "Running in shell: $OOS_SHELL";
+
+if ! shell_supported $OOS_SHELL; then
+	warning "Shell $OOS_SHELL is not supported.\nThe installation may misbehave.";
+fi
+
 # Next file
 . $@;
