727432c443a302b1669c0a03591d05bd14312b25
ilmikko <mikko@5x.fi>
2018-04-25 00:48:28 +0100

    Added grep to assertions

--- helpers.d/20-support.sh
+++ helpers.d/20-support.sh
@@ -1,9 +1,11 @@
 # Determines which shells are supported, for now.
 
+# Get the current shell that is running.
 get_shell() {
 	echo $(basename $(readlink /proc/$$/exe));
 }
 
+# Returns true if shell is supported.
 shell_supported() {
 	case "$@" in
 		"bash")
@@ -15,12 +17,15 @@ shell_supported() {
 	esac
 }
 
+# Check that every command we use is supported.
 check_support() {
 	assert_command readlink;
 	assert_command dirname;
 	assert_command basename;
+	assert_command grep;
 }
 
+# Abort if a given command is not supported.
 assert_command() {
 	if ! command_available "$@"; then
 		abort "Command $@ not supported.\nPlease check your \$PATH";
