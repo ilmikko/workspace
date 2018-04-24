aa4079c3caf8a43b63311e6b47793797664efa54
ilmikko <mikko@5x.fi>
2018-04-25 00:48:01 +0100

    Probe now warns if distribution is not supported

---
+++ helpers.d/30-probe.sh
@@ -0,0 +1,24 @@
+# Probing helpers
+
+# Current running distribution
+distribution() {
+	if command_available lsb_release; then
+		# Get the ID from lsb_release
+		release=$(lsb_release -a | grep -m 1 -i ID | grep -o '\w*$');
+		if ! [ "x$release" = "x" ]; then
+			echo $release;
+			return;
+		fi
+	fi
+
+	# Try to get the ID from etc file release
+	release=$(cat /etc/*release* | grep -m 1 -i ID | grep -o '\w*$');
+	if ! [ "x$release" = "x" ]; then
+		echo $release;
+		return;
+	fi
+
+	echo "unknown";
+}
+
+. $@;
