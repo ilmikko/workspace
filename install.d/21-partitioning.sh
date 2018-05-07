a0586d2432cac5d01935964ae52b4eecb6f54c92
ilmikko <mikko@5x.fi>
2018-05-08 00:42:57 +0100

    Added filesystem creation in addition to partitioning

--- install.d/21-partitioning.sh
+++ install.d/21-partitioning.sh
@@ -1,12 +1,16 @@
 oos_wipe() {
-	echo "Wiping $1...";
+	log "Wiping $1...";
 	wipefs -a $1;
 }
 
 oos_create_label() {
-	debug "Partition table label: $OOS_PARTITION_DISK_LABEL";
+	device=$1;
+	label=$2;
+	file=$3;
+
+	debug "Partition table label: $label";
 	# TODO: find a fdisk solution
-	parted $OOS_INSTALL_DEVICE --script mklabel $OOS_PARTITION_DISK_LABEL
+	parted $device --script mklabel $label
 }
 
 # Input: size
@@ -27,19 +31,21 @@ oos_create_partition() {
 }
 
 oos_partition_execute() {
-	file=$1;
+	device=$1;
+	file=$2;
 	echo w >> $file; # write
-	cat $file | fdisk $OOS_INSTALL_DEVICE;
+	cat $file | fdisk $device;
 	rm $file;
 }
 
 oos_partition() {
-	echo "Partitioning disk...";
+	log "Partitioning disk...";
+	device=$1;
 
 	# We need this to concatenate fdisk commands into one!
 	tempfile=$(mktemp /tmp/oos_fdisk.XXXXXX);
 
-	oos_create_label $tempfile;
+	oos_create_label $device $OOS_PARTITION_DISK_LABEL $tempfile;
 
 	# Partition it according to our partition settings in OOS_PARTITIONS
 	for (( i=0; i<${#OOS_PARTITIONS[@]}; i++ )) do
@@ -55,7 +61,48 @@ oos_partition() {
 		fi
 	done
 
-	oos_partition_execute $tempfile;
+	oos_partition_execute $device $tempfile;
+}
+
+# TODO: There needs to be a sanity check that mkfs.$filesystem exists before we install!
+oos_create_fs() {
+	fs_type=$1;
+	partition=$2;
+
+	case $fs_type in
+		"fat32")
+			mkfs.fat -F32 $partition;
+			#fatlabel $label;
+			;;
+		"swap")
+			mkswap $partition;
+			#swaplabel $label;
+			;;
+		"ext4"|"ext3"|"ext2")
+			mkfs.$fs_type -F $partition;
+			#e2label $label;
+		*)
+			yes | mkfs.$fs_type $partition;
+			;;
+	esac
+}
+
+oos_create_filesystems() {
+	device=$1;
+
+	# Get the partitions we just created
+	partitions=($(fdisk -l /dev/sdb | awk '/^\/dev/ { print $1 }'));
+
+	log "Creating filesystems...";
+	for (( i=0; i<${#OOS_PARTITIONS[@]}; i++ )) do
+		part=(${OOS_PARTITIONS[$i]//:/ });
+
+		filesystem=${part[3]};
+		partition=${partitions[$i]};
+
+		debug "Create fs $filesystem on $partition";
+		oos_create_fs $filesystem $partition;
+	done
 }
 
 # Wipe the device
@@ -65,7 +112,7 @@ oos_wipe $OOS_INSTALL_DEVICE;
 oos_partition $OOS_INSTALL_DEVICE;
 
 # Make the filesystems
-#oos_create_fs $OOS_INSTALL_DEVICE;
+oos_create_filesystems $OOS_INSTALL_DEVICE;
 
 # Mount the partitions, create the directory tree
 #oos_mount $OOS_INSTALL_DEVICE;
