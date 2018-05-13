10c07cebd40a75f293d8fe31c2e3742e44c2de19
ilmikko <mikko@5x.fi>
2018-05-13 02:37:52 +0100

    Small bugfixes, stage2 file removed after stage 2

--- install.d/stage0.d/99-choose.sh
+++ install.d/stage0.d/99-choose.sh
@@ -1,6 +1,12 @@
 log "Checking installation stage...";
 
 # TODO: Make this more sophisticated
-[ -a "stage3" ] && OOS_STAGE=stage3 || [ -a "stage2" ] && OOS_STAGE=stage2 || OOS_STAGE=stage1;
+if [ -a "stage3" ]; then
+	OOS_STAGE=stage3;
+elif [ -a "stage2"]; then
+	OOS_STAGE=stage2;
+else
+	OOS_STAGE=stage1;
+fi
 
 . $@ $OOS_INSTALL_PATH/$OOS_STAGE.d/*;
--- install.d/stage2.d/99-finish.sh
+++ install.d/stage2.d/99-finish.sh
@@ -2,6 +2,8 @@
 
 log "Stage 2 finished successfully";
 
+rm stage2;
+
 [ "$OOS_USE_REBOOT" = 1 ] && log "Rebooting..." && reboot;
 
 exit;
