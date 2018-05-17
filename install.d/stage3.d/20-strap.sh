e840750652eb0217aba1e9db2a74e6a5b0c3d220
ilmikko <mikko@5x.fi>
2018-05-17 20:43:25 +0100

    Fix pacman confirmation dialogs

--- install.d/stage3.d/20-strap.sh
+++ install.d/stage3.d/20-strap.sh
@@ -1,7 +1,7 @@
 # Install any additional packages now that we have booted and connected to the internet
 
 oos_install_strap() {
-	pacman -S $OOS_INSTALL_PACKAGES;
+	pacman --noconfirm -S $OOS_INSTALL_PACKAGES;
 }
 
 oos_install_strap;
